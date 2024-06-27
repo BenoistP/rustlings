1e8f3ab5093f5d914e7f1fe4a992d791b587d883
BenoistP <bp_github.com@gazouille.net>
2024-06-27 17:07:34 +0200

    exercices résolus

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -10,10 +10,19 @@
 
 #[test]
 fn main() {
+    // let mut x = 100;
+    // let y = &mut x;
+    // let z = &mut x;
+    // *y += 100;
+    // *z += 1000;
+    // assert_eq!(x, 1200);
+
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+
+    let z = &mut y;
     *z += 1000;
     assert_eq!(x, 1200);
+
 }
